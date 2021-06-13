77e944f83cc2283e586dd559897683f961015141
nissy-dev <nd.12021218@gmail.com>
2021-06-13 15:43:26 +0900

    feat(linked list): refactor test

--- too-many-linked-list/singly-linked-list/src/lib.rs
+++ too-many-linked-list/singly-linked-list/src/lib.rs
@@ -45,10 +45,10 @@ impl SinglyLinkedList {
 
     // O(1)
     pub fn pop(&mut self) -> Option<usize> {
-        self.size -= 1;
         match self.head.take() {
             None => None,
             Some(node) => {
+                self.size -= 1;
                 self.head = node.next;
                 Some(node.value)
             }
@@ -57,10 +57,8 @@ impl SinglyLinkedList {
 
     // O(n)
     pub fn insert(&mut self, index: usize, value: usize) -> Option<usize> {
-        self.size += 1;
-
         // 最後に挿入するとき
-        if index + 2 == self.size() {
+        if index == self.size() {
             self.push_back(value);
             return Some(value);
         }
@@ -75,6 +73,7 @@ impl SinglyLinkedList {
                         value: value,
                         next: curr_node.take(),
                     }));
+                    self.size += 1;
                     return Some(value);
                 }
                 Some(boxed_node) => {
@@ -88,7 +87,6 @@ impl SinglyLinkedList {
 
     // O(n)
     pub fn delete(&mut self, index: usize) -> Option<usize> {
-        self.size -= 1;
         let mut curr_node = &mut self.head;
         let mut cnt: usize = 0;
         loop {
@@ -97,6 +95,7 @@ impl SinglyLinkedList {
                 Some(boxed_node) if cnt == index => {
                     let removed_value = boxed_node.value;
                     *curr_node = boxed_node.next.take();
+                    self.size -= 1;
                     return Some(removed_value);
                 }
                 Some(boxed_node) => {
@@ -110,13 +109,13 @@ impl SinglyLinkedList {
 
     // O(n)
     pub fn delete_by_value(&mut self, value: usize) -> Option<usize> {
-        self.size -= 1;
         let mut curr_node = &mut self.head;
         loop {
             match curr_node {
                 None => break,
                 Some(boxed_node) if boxed_node.value == value => {
                     *curr_node = boxed_node.next.take();
+                    self.size -= 1;
                     return Some(value);
                 }
                 Some(boxed_node) => {
@@ -195,12 +194,21 @@ impl Drop for SinglyLinkedList {
     }
 }
 
+pub fn setup() -> SinglyLinkedList {
+    let mut list = SinglyLinkedList::new();
+    list.push_front(0);
+    list.push_front(1);
+    list.push_front(2);
+    return list;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::setup;
     use super::SinglyLinkedList;
 
     #[test]
-    fn linked_list_test() {
+    fn push() {
         let mut list = SinglyLinkedList::new();
 
         list.push_front(1);
@@ -209,23 +217,38 @@ mod tests {
         assert_eq!(list.size(), 3);
         // 2が先頭、0が末端
         assert_eq!(list.show(), "2 -> 1 -> 0 -> None");
+    }
 
-        // reverse
+    #[test]
+    fn reverse() {
+        let mut list = setup();
         list.reverse();
         assert_eq!(list.show(), "0 -> 1 -> 2 -> None");
-        list.reverse();
+    }
 
-        // pop
+    #[test]
+    fn pop() {
+        let mut list = setup();
         assert_eq!(list.pop(), Some(2));
         assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), Some(0));
+        assert_eq!(list.pop(), None);
+        assert_eq!(list.pop(), None);
+        assert_eq!(list.size(), 0);
+    }
 
-        // get
-        list.push_front(1);
-        list.push_front(2);
+    #[test]
+    fn get() {
+        let list = setup();
         assert_eq!(list.get(0), Some(2));
         assert_eq!(list.get(1), Some(1));
         assert_eq!(list.get(2), Some(0));
         assert_eq!(list.get(3), None);
+    }
+
+    #[test]
+    fn insert() {
+        let mut list = setup();
 
         // insert
         list.insert(1, 5);
@@ -233,19 +256,35 @@ mod tests {
         assert_eq!(list.insert(7, 3), None);
         list.insert(4, 3);
         assert_eq!(list.show(), "2 -> 5 -> 1 -> 0 -> 3 -> None");
+        assert_eq!(list.size(), 5);
+    }
+
+    #[test]
+    fn delete() {
+        let mut list = setup();
 
         // delete
-        assert_eq!(list.delete(2), Some(1));
-        assert_eq!(list.show(), "2 -> 5 -> 0 -> 3 -> None");
+        assert_eq!(list.delete(2), Some(0));
+        assert_eq!(list.show(), "2 -> 1 -> None");
         assert_eq!(list.delete(5), None);
         assert_eq!(list.delete(6), None);
+        assert_eq!(list.size(), 2);
+    }
+
+    #[test]
+    fn delete_by_value() {
+        let mut list = setup();
 
         // delete by value
-        list.delete_by_value(5);
-        assert_eq!(list.show(), "2 -> 0 -> 3 -> None");
+        list.delete_by_value(1);
+        assert_eq!(list.show(), "2 -> 0 -> None");
         assert_eq!(list.delete_by_value(10), None);
+        assert_eq!(list.size(), 2);
+    }
 
-        // clear
+    #[test]
+    fn clear() {
+        let mut list = setup();
         list.clear();
         assert_eq!(list.size(), 0);
     }
