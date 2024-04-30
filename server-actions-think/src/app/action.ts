7223dbe4b68860ebebcd1bc350b5f7e718caedef
nissy-dev <nd.12021218@gmail.com>
2024-04-30 21:52:16 +0900

    add

--- server-actions-think/src/app/action.ts
+++ server-actions-think/src/app/action.ts
@@ -1,9 +1,12 @@
 "use server";
 
+import { redirect } from "next/navigation";
+
 const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 
 export const createTodo = async () => {
   await sleep(2000);
   throw new Error("Server action failed");
   // return "Todo created";
+  // redirect("/todo");
 };
