df76abe847fe2c33dd7e7edd1a363ea0ccaca580
nissy-dev <nd.12021218@gmail.com>
2024-07-19 19:11:28 +0900

    add

--- migration-tests/drizzle/src/schema.ts
+++ migration-tests/drizzle/src/schema.ts
@@ -3,6 +3,7 @@ import { int, mysqlTable, text } from "drizzle-orm/mysql-core";
 export const user = mysqlTable("user", {
   id: int("id").primaryKey().autoincrement(),
   name: text("name"),
+  age: int("age"),
 });
 
 export const book = mysqlTable("book", {
--- migration-tests/drizzle/src/script.ts
+++ migration-tests/drizzle/src/script.ts
@@ -4,15 +4,10 @@ import { book, user } from "./schema";
 async function main() {
   await db.insert(user).values({
     name: "Bob",
+    age: 20,
   });
-  const data1 = await db.query.user.findFirst().execute();
-  console.log(data1);
-  await db.insert(book).values({
-    name: "book1",
-    authorId: data1?.id,
-  });
-  const data2 = await db.query.book.findFirst().execute();
-  console.log(data2);
+  const data = await db.query.user.findMany().execute();
+  console.log(data);
 }
 
 main()
