5e6e195a33fb2ccf0e4103903ef807c9218e32c2
nissy-dev <nd.12021218@gmail.com>
2025-11-30 17:57:52 +0900

    update

--- go-crd-operation/scripts/codegen.sh
+++ go-crd-operation/scripts/codegen.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-set -o errexit
-set -o nounset
-set -o pipefail
-
 SCRIPT_DIR="$(dirname "${BASH_SOURCE[0]}")"
 SCRIPT_ROOT="${SCRIPT_DIR}/.."
 CODEGEN_PKG=$(go list -m -f '{{.Dir}}' k8s.io/code-generator)
@@ -21,7 +17,6 @@ kube::codegen::gen_register \
     "${SCRIPT_ROOT}"
 
 kube::codegen::gen_client \
-    --with-watch \
     --output-dir "${SCRIPT_ROOT}/pkg/generated" \
     --output-pkg "${THIS_PKG}/pkg/generated" \
     --boilerplate "${SCRIPT_ROOT}/scripts/boilerplate.go.txt" \
